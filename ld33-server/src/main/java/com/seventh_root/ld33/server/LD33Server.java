b75bd0256c9c29945b7cdb0d64a051be65ae71f2
Ross Binden <alyphen@gmail.com>
2015-08-22 04:34:55 +0100

    Adds game loops, configuration loading and logging to server

--- ld33-server/src/main/java/com/seventh_root/ld33/server/LD33Server.java
+++ ld33-server/src/main/java/com/seventh_root/ld33/server/LD33Server.java
@@ -16,6 +16,128 @@
 
 package com.seventh_root.ld33.server;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static java.lang.System.currentTimeMillis;
+import static java.util.logging.Level.SEVERE;
+
 public class LD33Server {
+
+    private Map<String, Object> config;
+    private Logger logger;
+    private boolean running;
+    private static final long DELAY = 25L;
+
+    public static void main(String[] args) {
+        new LD33Server().start();
+    }
+
+    public Map<String, Object> getConfig() {
+        return config;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public LD33Server() {
+        loadConfig();
+        logger = Logger.getLogger(getClass().getCanonicalName());
+    }
+
+    public void start() {
+        setRunning(true);
+        long beforeTime, timeDiff, sleep;
+        beforeTime = currentTimeMillis();
+        while (isRunning()) {
+            doTick();
+            timeDiff = currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+            if (sleep > 0) {
+                try {
+                    Thread.sleep(sleep);
+                } catch (InterruptedException exception) {
+                    getLogger().log(SEVERE, "Thread interrupted", exception);
+                }
+            }
+            beforeTime = currentTimeMillis();
+        }
+    }
+
+    private void doTick() {
+
+    }
+
+    public void loadConfig() {
+        File configFile = new File("./config.json");
+        try {
+            saveDefaultConfig(configFile);
+        } catch (IOException exception) {
+            getLogger().log(SEVERE, "Failed to create default config", exception);
+        }
+        try {
+            Reader reader = new FileReader(configFile);
+            Gson gson = new Gson();
+            config = gson.fromJson(reader, new TypeToken<HashMap<String, Object>>() {
+            }.getType());
+        } catch (FileNotFoundException exception) {
+            getLogger().log(SEVERE, "Failed to find configuration", exception);
+        }
+    }
+
+    public void saveDefaultConfig(File configFile) throws IOException {
+        if (!configFile.getParentFile().isDirectory()) {
+            if (!configFile.getParentFile().delete()) {
+                throw new IOException("Failed to remove " + configFile.getParentFile().getPath() + ": do you have permission?");
+            }
+        }
+        if (!configFile.getParentFile().exists()) {
+            if (!configFile.getParentFile().mkdirs()) {
+                throw new IOException("Failed to create directory " + configFile.getParentFile().getPath() + ": do you have permission?");
+            }
+        }
+        if (!configFile.exists()) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            Map<String, Object> defaultConfig = new HashMap<>();
+            defaultConfig.put("port", "37896");
+            gson.toJson(defaultConfig, new FileWriter(configFile));
+        }
+    }
+
+    public void saveDefaultConfig() throws IOException {
+        saveDefaultConfig(new File("./config.json"));
+    }
+
+    public void saveConfig(File configFile) throws IOException {
+        if (!configFile.getParentFile().isDirectory()) {
+            if (!configFile.getParentFile().delete()) {
+                throw new IOException("Failed to remove " + configFile.getParentFile().getPath() + ": do you have permission?");
+            }
+        }
+        if (!configFile.getParentFile().exists()) {
+            if (!configFile.getParentFile().mkdirs()) {
+                throw new IOException("Failed to create directory " + configFile.getParentFile().getPath() + ": do you have permission?");
+            }
+        }
+        if (!configFile.exists()) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(config, new FileWriter(configFile));
+        }
+    }
 
 }
